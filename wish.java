a7139083d481daa1c0afa2a88083d4c6d9675eaf
Pavansai1992 <pavansai92@outlook.com>
2024-08-24 20:32:08 +0530

    wish.java

---
+++ wish.java
@@ -0,0 +1,17 @@
+---
+id: 101
+Name: Sairam
+gender: Male
+Phno: 123456
+Job:
+  company:Cisco 
+  exp: 8 years
+  Skill: AWS
+Hobbies:
+  - Cricket
+  - Chess
+  - Music
+Address:
+  City: Guntur
+  State: AP
+  Country: India
