dc95a5e2430cdaeb91e9b562ac3bcf5556a29d21
Pavansai1992 <pavansai92@outlook.com>
2024-08-25 09:40:09 +0530

    welcome.java task 101 is updated

--- welcome.java
+++ welcome.java
@@ -5,8 +5,18 @@ After I start the docker and kubernetes
 
 Then I will go for AWS
 
+<<<<<<< Updated upstream
 
 
 task 120 is in progress
 j=20
 i=30
+=======
+task 101 is in progress
+i=20
+j=30
+
+
+
+
+>>>>>>> Stashed changes
